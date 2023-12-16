edcbc0c56e4e7bb5e3df1aff26c88ac3d9a1c0cb
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-12-16 23:55:06 +0100

    Create cards.js

---
+++ src/cards.js
@@ -0,0 +1,28 @@
+export function createCard(params) {
+  return params
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `
+            <div class="photo-card">
+              <a href="${largeImageURL}">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300px" />
+                <div class="info">
+                  <p class="info-item"><b>Likes</b><br/><b>${likes}</b></p>
+                  <p class="info-item"><b>Views</b><br/><b>${views}</b></p>
+                  <p class="info-item"><b>Comments</b><br/><b>${comments}</b></p>
+                  <p class="info-item"><b>Downloads</b><br/><b>${downloads}</b></p>
+                </div>
+              </a>
+            </div>
+              `
+    )
+    .join('');
+}
