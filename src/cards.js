76fc6f4ffc6f288aca5e72697c6ce939ae050c5f
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-12-18 11:37:01 +0100

    Update cards.js

--- src/cards.js
+++ src/cards.js
@@ -2,8 +2,8 @@ export function createMarkup(arr) {
   return arr
     .map(
       ({
-        largeImageURL,
         webformatURL,
+        largeImageURL,
         tags,
         likes,
         views,
@@ -13,8 +13,8 @@ export function createMarkup(arr) {
         return;
         `
             <div class="photo-card">
-              <a href="${largeImageURL}">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300px" />
+              <a href="${webformatURL}">
+                <img src="${largeImageURL}" alt="${tags}" loading="lazy" width="300px" />
                 <div class="info">
                   <p class="info-item"><b>Likes</b><br/><b>${likes}</b></p>
                   <p class="info-item"><b>Views</b><br/><b>${views}</b></p>
