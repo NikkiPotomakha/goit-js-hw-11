ffffea154183b2762252a32bd4752f649fe958b1
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-12-18 11:36:54 +0100

    Update simplelightbox.js

--- src/simplelightbox.js
+++ src/simplelightbox.js
@@ -1,8 +1,8 @@
-import SimpleLightbox from 'simplelightbox';
+//import SimpleLightbox from 'simplelightbox';
 
-import 'simplelightbox/dist/simple-lightbox.min.css';
+//import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+//export let lightbox = new SimpleLightbox('.gallery a', {
+//captionsData: 'alt',
+//captionDelay: 250,
+//});
